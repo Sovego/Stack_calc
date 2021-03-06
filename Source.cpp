259625e7becdac52be7dcd91b889999ce981ab4b
Егор Софронов <egor.sofronov03@gmail.com>
2021-03-06 21:45:23 +0700

    работает вычисление однозначных чисел без скобок. Не работает если результат выражения не однозначный.

--- Source.cpp
+++ Source.cpp
@@ -8,6 +8,14 @@ struct Node
     Node* next;
 };
 
+int ChartoInt(char a)
+{
+	if ((int)a > 0) return (int)a - 48;
+}
+char InttoChar(int a)
+{
+	if (a >= 0 && a < 10) return (char)a + 48;
+}
 bool is_empty(Node* top)
 {
 	return top==nullptr;
@@ -72,7 +80,6 @@ int main()
 	Node* top=nullptr;
 	std::cout << "Input ";
 	std::cin >> in_str;
-	int b{0};
 	for (int i=0;i < in_str.length();i++)
 	{
 		switch(in_str[i])
@@ -80,11 +87,12 @@ int main()
 		case '+': if (is_empty(top))
 			{
 				top=make_stack(in_str[i]);
-			b=1;
+			
 			} else if(top->value=='*' || top->value=='/' || top->value=='+' || top->value=='-')
 			{
-				out_str+=top->value;
 				out_str+=" ";
+				out_str+=top->value;
+				
 				top=del_top(top);
 				if(is_empty(top))
 				{
@@ -99,11 +107,12 @@ int main()
 		case '-': if (is_empty(top))
 			{
 				top=make_stack(in_str[i]);
-			b=1;
+			
 			} else if(top->value=='*' || top->value=='/' || top->value=='+' || top->value=='-')
 			{
-				out_str+=top->value;
 				out_str+=" ";
+				out_str+=top->value;
+				
 				top=del_top(top);
 				if(is_empty(top))
 				{
@@ -118,12 +127,13 @@ int main()
 		case '*': if (is_empty(top))
 			{
 				top=make_stack(in_str[i]);
-			b=1;
+			
 			} else if(top->value=='*' || top->value=='/')
 			{
+				out_str+=" ";
 				out_str+=top->value;
 				top=del_top(top);
-				out_str+=" ";
+				
 				if(is_empty(top))
 				{
 					top=make_stack(in_str[i]);
@@ -137,11 +147,12 @@ int main()
 		case '/': if (is_empty(top))
 			{
 				top=make_stack(in_str[i]);
-			b=1;
+			
 			} else if(top->value=='*' || top->value=='/')
 			{
-				out_str+=top->value;
 				out_str+=" ";
+				out_str+=top->value;
+				
 				top=del_top(top);
 				if(is_empty(top))
 				{
@@ -156,7 +167,7 @@ int main()
 		case '(': if (is_empty(top))
 			{
 				top=make_stack(in_str[i]);
-			b=1;
+			
 			} else
 			{
 				top=add_element_top(top,in_str[i]);
@@ -169,9 +180,10 @@ int main()
 		}
 			while (top->value!='(')
 			{
+				out_str+=" ";
 				out_str+=top->value;
 				top=del_top(top);
-				out_str+=" ";
+				
 			}
 			top=del_top(top);
 			break;
@@ -179,14 +191,62 @@ int main()
 		{
 			out_str+=in_str[i];
 			out_str+=" ";
-		} else out_str+=in_str[i];
+		} else 
+		{
+			
+			out_str+=in_str[i];
+			out_str+=" ";
 		}
 	}
 	while (top!=nullptr)
 	{
+		out_str+=" ";
 		out_str+=top->value;
+		
 		top=del_top(top);
 	}
 	std::cout << std::endl << out_str;
+	
+	for (int i=0;i < out_str.length();i++)
+	{
+		if (out_str[i]=='*' || out_str[i]=='/' || out_str[i]=='+' || out_str[i]=='-')
+		{
+			int a{ChartoInt(top->value)};
+			top=del_top(top);
+			int b{ChartoInt(top->value)};
+			top=del_top(top);
+			switch (out_str[i])
+			{
+			case '+':top=add_element_top(top,InttoChar(a+b));
+					break;
+			case '-':top=add_element_top(top,InttoChar(a-b));
+					break;
+			case '*':top=add_element_top(top,InttoChar(a*b));
+					break;
+			case '/':top=add_element_top(top,InttoChar(a/b));
+					break;
+			}
+		} else if (out_str[i]!=' ')
+			if(out_str[i+1]==' ')
+		{
+			if (is_empty(top))
+			{
+				top=make_stack(out_str[i]);
+			} else
+				top=add_element_top(top,out_str[i]);
+			
+		} else
+		{
+			if (is_empty(top))
+			{
+				top=make_stack(out_str[i]+out_str[i+1]);
+			} else
+			top=add_element_top(top,out_str[i]+out_str[i+1]);
+			out_str[i+1]=' ';
+		}
+	}
+	std::cout << std::endl << top->value;
+}
+
 }
 
